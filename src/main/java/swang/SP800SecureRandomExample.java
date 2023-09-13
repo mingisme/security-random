5188da700539a76faa5de2312132d04db9644834
ming.wang4 <ming.wang4@envision-digital.com>
2023-09-13 17:43:00 +0800

    SP800SecureRandom Example

---
+++ src/main/java/swang/SP800SecureRandomExample.java
@@ -0,0 +1,27 @@
+package swang;
+
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.prng.SP800SecureRandom;
+import org.bouncycastle.crypto.prng.SP800SecureRandomBuilder;
+import org.bouncycastle.crypto.prng.FixedSecureRandom;
+
+import java.security.SecureRandom;
+
+public class SP800SecureRandomExample {
+    public static void main(String[] args) {
+        byte[] seed = new byte[128];
+        SecureRandom basic = new SecureRandom();
+        basic.nextBytes(seed);
+        SP800SecureRandomBuilder sp800SecureRandomBuilder = new SP800SecureRandomBuilder(new FixedSecureRandom(seed), false);
+        SP800SecureRandom random = sp800SecureRandomBuilder.buildHMAC(new HMac(new SHA256Digest()), seed, false);
+        for (int i = 0; i < 100; i++) {
+            int j = random.nextInt();
+            System.out.println(j);
+            byte[] bytes = new byte[128];
+            random.nextBytes(bytes);
+            System.out.println(new String(bytes));
+        }
+
+    }
+}
